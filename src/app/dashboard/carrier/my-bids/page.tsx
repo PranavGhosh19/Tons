2ad8585e0d91e76b42f485d494df09f92f9adbcd
Pranav Ghosh <pranavghosh19@gmail.com>
2025-08-20 06:52:25 +0000

    For Carriers, create a "My Bids" page!

--- src/app/dashboard/carrier/my-bids/page.tsx
+++ src/app/dashboard/carrier/my-bids/page.tsx
@@ -13,6 +13,7 @@ import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 type BidWithShipment = {
   bid: DocumentData;
